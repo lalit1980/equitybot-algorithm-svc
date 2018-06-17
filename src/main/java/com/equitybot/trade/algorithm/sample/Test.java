4103feace00885361f4725c4691f2d95f09f0ebd
Anil Bhagat <anilbhagat@Anils-MacBook-Pro.local>
2018-06-17 12:43:39 +0800

    test

--- src/main/java/com/equitybot/trade/algorithm/sample/Test.java
+++ src/main/java/com/equitybot/trade/algorithm/sample/Test.java
@@ -3,8 +3,13 @@ package com.equitybot.trade.algorithm.sample;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.cache.CacheAtomicityMode;
+import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.cache.CacheRebalanceMode;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.kubernetes.TcpDiscoveryKubernetesIpFinder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -23,9 +28,21 @@ public class Test {
 
 	public Test() {
 		IgniteConfiguration cfg = new IgniteConfiguration();
+		TcpDiscoverySpi tcpDiscoverySpi= new TcpDiscoverySpi();
+		TcpDiscoveryKubernetesIpFinder ipFinder  = new TcpDiscoveryKubernetesIpFinder();
+		ipFinder.setServiceName("ignite");
+		tcpDiscoverySpi.setIpFinder(ipFinder);
+		cfg.setDiscoverySpi(tcpDiscoverySpi);
+		cfg.setPeerClassLoadingEnabled(true);
+		cfg.setClientMode(true);
 		Ignite ignite = Ignition.start(cfg);
 		Ignition.setClientMode(true);
+		ignite.active(true);
 		CacheConfiguration<String, TimeSeries> ccfg = new CacheConfiguration<String, TimeSeries>("TimeSeriesCache");
+		ccfg.setAtomicityMode(CacheAtomicityMode.ATOMIC);
+		ccfg.setCacheMode(CacheMode.PARTITIONED);
+		ccfg.setRebalanceMode(CacheRebalanceMode.NONE);
+		ccfg.setDataRegionName("1GB_Region");
 		this.cache = ignite.getOrCreateCache(ccfg);
 	}
 
