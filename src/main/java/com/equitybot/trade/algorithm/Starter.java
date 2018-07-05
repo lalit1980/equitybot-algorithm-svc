d8703ee1436d084ac7c7534bd2440faf617b9968
Lalit <lalit1980@gmail.com>
2018-07-05 15:14:40 +0800

    test

--- src/main/java/com/equitybot/trade/algorithm/Starter.java
+++ src/main/java/com/equitybot/trade/algorithm/Starter.java
@@ -1,11 +1,7 @@
 package com.equitybot.trade.algorithm;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.equitybot.trade.algorithm.mongodb.repository.ActionLogDataRepository;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -14,8 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 public class Starter {
 	
-	 @Autowired
-	    private ActionLogDataRepository actionRepo;
+	
 	public static void main(String[] args){
 		SpringApplication.run(Starter.class, args);
 		
