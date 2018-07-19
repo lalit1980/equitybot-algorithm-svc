c6fd894106b4cccc20b93ffd80b839363a902542
Lalit Singh <lalit1980@gmail.com>
2018-07-19 20:57:57 +0800

    Testing

--- src/main/java/com/equitybot/trade/algorithm/ignite/configs/IgniteConfig.java
+++ src/main/java/com/equitybot/trade/algorithm/ignite/configs/IgniteConfig.java
@@ -3,6 +3,8 @@ package com.equitybot.trade.algorithm.ignite.configs;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
 import org.apache.ignite.configuration.IgniteConfiguration;
+import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
+import org.apache.ignite.spi.discovery.tcp.ipfinder.kubernetes.TcpDiscoveryKubernetesIpFinder;
 import org.springframework.stereotype.Service;
 @Service
 public class IgniteConfig {
@@ -13,6 +15,15 @@ public class IgniteConfig {
 	static{
         try{
         	IgniteConfiguration cfg = new IgniteConfiguration();
+            TcpDiscoverySpi tcpDiscoverySpi= new TcpDiscoverySpi();
+            TcpDiscoveryKubernetesIpFinder ipFinder  = new TcpDiscoveryKubernetesIpFinder();
+            ipFinder.setServiceName("ignite");
+            ipFinder.setMasterUrl("https://api.apsouth.dev.etradingparagon.com");
+
+            tcpDiscoverySpi.setIpFinder(ipFinder);
+            cfg.setDiscoverySpi(tcpDiscoverySpi);
+            cfg.setPeerClassLoadingEnabled(true);
+            cfg.setClientMode(true);
         	ignite = Ignition.start(cfg);
         	Ignition.setClientMode(true);
         }catch(Exception e){
--- src/main/java/com/equitybot/trade/algorithm/util/Cache.java
+++ src/main/java/com/equitybot/trade/algorithm/util/Cache.java
@@ -3,6 +3,9 @@ package com.equitybot.trade.algorithm.util;
 import com.equitybot.trade.algorithm.ignite.configs.IgniteConfig;
 import com.zerodhatech.models.Tick;
 import org.apache.ignite.IgniteCache;
+import org.apache.ignite.cache.CacheAtomicityMode;
+import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.cache.CacheRebalanceMode;
 import org.apache.ignite.configuration.CacheConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
@@ -27,12 +30,25 @@ public class Cache {
 
     public Cache(){
         CacheConfiguration<Long, String> ccfgOrderDetails = new CacheConfiguration<Long, String>("CachedTradeOrder");
+        ccfgOrderDetails.setAtomicityMode(CacheAtomicityMode.ATOMIC);
+        ccfgOrderDetails.setCacheMode(CacheMode.PARTITIONED);
+        ccfgOrderDetails.setRebalanceMode(CacheRebalanceMode.NONE);
+        ccfgOrderDetails.setDataRegionName("1GB_Region");
+
         this.boughtInstruments = igniteConfig.getInstance().getOrCreateCache(ccfgOrderDetails);
 
         CacheConfiguration<String, TimeSeries> ccfg = new CacheConfiguration<String, TimeSeries>("TimeSeriesCache");
+        ccfg.setAtomicityMode(CacheAtomicityMode.ATOMIC);
+        ccfg.setCacheMode(CacheMode.PARTITIONED);
+        ccfg.setRebalanceMode(CacheRebalanceMode.NONE);
+        ccfg.setDataRegionName("1GB_Region");
         this.timeSeries = igniteConfig.getInstance().getOrCreateCache(ccfg);
 
         CacheConfiguration<Long, Tick> ccfgLatestTickParams = new CacheConfiguration<>("CachedLatestTick");
+        ccfgLatestTickParams.setAtomicityMode(CacheAtomicityMode.ATOMIC);
+        ccfgLatestTickParams.setCacheMode(CacheMode.PARTITIONED);
+        ccfgLatestTickParams.setRebalanceMode(CacheRebalanceMode.NONE);
+        ccfgLatestTickParams.setDataRegionName("1GB_Region");
         this.cacheLatestTick = igniteConfig.getInstance().getOrCreateCache(ccfgLatestTickParams);
     }
 
